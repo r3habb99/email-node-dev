67a6f0502ae4f8c4b2bf1ed6dfbdb57c5f0238b4
Rishabh Prajapati <rishabh.prajapati@brainvire.com>
2024-07-22 12:39:44 +0530

    updated few path

--- src/utils/sendClosePageScript.utils.js
+++ src/utils/sendClosePageScript.utils.js
@@ -2,7 +2,7 @@ const path = require('path');
 const fs = require('fs');
 const logger = require('../utils/logger.utils');
 module.exports = function sendClosePageScript(res) {
-  const filePath = path.join(__dirname, '..', 'views', 'closePage.html');
+  const filePath = path.join(__dirname, '../../', 'views', 'closePage.html');
 
   fs.readFile(filePath, 'utf8', (err, data) => {
     if (err) {
